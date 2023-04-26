640a925186ca812a61bbcefd801121d4c5a4b2d8
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-04-26 18:55:25 +0300

    Added methods for getting owner and salesman vehicles

--- CarSales.Core/Contracts/IVehicleService.cs
+++ CarSales.Core/Contracts/IVehicleService.cs
@@ -1,12 +1,36 @@
 ﻿using CarSales.Core.Models.Vehicles;
+using CarSales.Infrastructure.Data.Entities;
 
 namespace CarSales.Core.Contracts
 {
     public interface IVehicleService
     {
-        Task<ICollection<VehicleListViewModel>> GetAllVehiclesWhichAreForSale();
+        /// <summary>
+        /// Returns all <see cref="Vehicle"/> which are for sale
+        /// </summary>
+        /// <returns><see cref="ICollection{VehicleListViewModel}"/></returns>
+        Task<ICollection<VehicleListViewModel>> GetAllVehiclesWhichAreForSaleAsync();
 
-        Task<VehicleViewModel> GetVehicleById(int id);
+        /// <summary>
+        /// Returns <see cref="Vehicle"/> by given <paramref name="id"/>
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns><see cref="VehicleViewModel"/></returns>
+        Task<VehicleViewModel> GetVehicleByIdAsync(int id);
+
+        /// <summary>
+        /// Returns all <see cref="Vehicle"/> to <see cref="Owner"/> with given <paramref name="userId"/>
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns><see cref="ICollection{VehicleListViewModel}"/></returns>
+        Task<ICollection<VehicleListViewModel>> GetOwnerVehiclesAsync(string userId);
+
+        /// <summary>
+        /// Returns all <see cref="Vehicle"/> to <see cref="Salesman"/> with given <paramref name="userId"/>
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns><see cref="ICollection{VehicleListViewModel}"/></returns>
+        Task<ICollection<VehicleListViewModel>> GetSalesmanVehiclesAsync(string userId);
 
     }
 }
---
+++ CarSales.Core/Services/VehicleService.cs
@@ -0,0 +1,97 @@
+﻿using AirsoftMatchMaker.Infrastructure.Data.Common.Repository;
+using CarSales.Core.Contracts;
+using CarSales.Core.Models.Vehicles;
+using CarSales.Infrastructure.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarSales.Core.Services
+{
+    public class VehicleService : IVehicleService
+    {
+        private readonly IRepository repository;
+        public VehicleService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+        public async Task<ICollection<VehicleListViewModel>> GetAllVehiclesWhichAreForSaleAsync()
+        {
+            var vehicles =await repository.AllReadOnly<Vehicle>()
+                .Where(v => v.OwnerId == null)
+                .Select(v => new VehicleListViewModel()
+                {
+                    Id = v.Id,
+                    Name = $"{v.Brand} {v.Model} {v.YearProduced}",
+                    ImageUrl = v.ImageUrl,
+                    VehicleType = v.VehicleType,
+                    VehicleRating = v.VehicleRating,
+                    Price = v.Price,
+                    SalesmanName = $"{v.Salesman.User.FirstName} {v.Salesman.User.LastName}"
+                })
+                .ToListAsync();
+            return vehicles;
+        }
+
+        public async Task<VehicleViewModel> GetVehicleByIdAsync(int id)
+        {
+            var vehicle = await repository.AllReadOnly<Vehicle>()
+                .Where(v => v.Id == id)
+                .Select(v => new VehicleViewModel()
+                {
+                    Id = v.Id,
+                    Brand = v.Brand,
+                    Model = v.Model,
+                    Description = v.Description,
+                    ImageUrl = v.ImageUrl,
+                    YearProduced = v.YearProduced,
+                    TopSpeed = v.TopSpeed,
+                    KilometersDriven = v.KilometersDriven,
+                    Price = v.Price,
+                    VehicleType = v.VehicleType,
+                    VehicleRating = v.VehicleRating,
+                    SalesmanId = v.SalesmanId,
+                    SalesmanName = v.Salesman != null ?  $"{v.Salesman.User.FirstName} {v.Salesman.User.LastName}" : null,
+                    OwnerId = v.OwnerId,
+                    OwnerName = v.Owner != null ? $"{v.Owner.User.FirstName} {v.Owner.User.LastName}" : null
+                }).FirstOrDefaultAsync();
+            return vehicle;
+        }
+
+        public async Task<ICollection<VehicleListViewModel>> GetOwnerVehiclesAsync(string userId)
+        {
+            var owner = await repository.AllReadOnly<Owner>()
+                .FirstOrDefaultAsync(o => o.UserId == userId);
+            var ownerVehicles = await repository.AllReadOnly<Vehicle>()
+                .Where(v => v.OwnerId == owner.Id).
+                Select(v => new VehicleListViewModel()
+                {
+                    Id = v.Id,
+                    Name = $"{v.Brand} {v.Model} {v.YearProduced}",
+                    ImageUrl = v.ImageUrl,
+                    VehicleType = v.VehicleType,
+                    VehicleRating = v.VehicleRating,
+                    Price = v.Price,
+                    SalesmanName = $"{v.Salesman.User.FirstName} {v.Salesman.User.LastName}"
+                }).ToListAsync();
+            return ownerVehicles;
+        }
+
+        public async Task<ICollection<VehicleListViewModel>> GetSalesmanVehiclesAsync(string userId)
+        {
+            var salesman = await repository.AllReadOnly<Salesman>()
+                .FirstOrDefaultAsync(o => o.UserId == userId);
+            var salesmanVehicles = await repository.AllReadOnly<Vehicle>()
+                .Where(v => v.OwnerId == salesman.Id).
+                Select(v => new VehicleListViewModel()
+                {
+                    Id = v.Id,
+                    Name = $"{v.Brand} {v.Model} {v.YearProduced}",
+                    ImageUrl = v.ImageUrl,
+                    VehicleType = v.VehicleType,
+                    VehicleRating = v.VehicleRating,
+                    Price = v.Price,
+                    SalesmanName = $"{v.Salesman.User.FirstName} {v.Salesman.User.LastName}"
+                }).ToListAsync();
+            return salesmanVehicles;
+        }
+    }
+}
