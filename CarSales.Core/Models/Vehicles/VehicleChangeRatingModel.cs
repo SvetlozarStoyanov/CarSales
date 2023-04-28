0b625bf7a8df6bd6d2d528598274bf9a6aa4e53c
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-04-28 16:35:37 +0300

    Added model for changing rating

---
+++ CarSales.Core/Models/Vehicles/VehicleChangeRatingModel.cs
@@ -0,0 +1,11 @@
+﻿using CarSales.Infrastructure.Data.Enums;
+
+namespace CarSales.Core.Models.Vehicles
+{
+    public class VehicleChangeRatingModel
+    {
+        public int Id { get; set; }
+        public VehicleRating OldRating { get; set; }
+        public VehicleRating NewRating { get; set; }
+    }
+}
