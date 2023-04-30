6e652b1a738fb8f43d8d649e9571945c7d2a9b66
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-04-30 15:44:34 +0300

    Added salesman service which has methods for adding new salesman to db, or marking him as inactive if he leaves salesman role.

---
+++ CarSales.Core/Contracts/ISalesmanService.cs
@@ -0,0 +1,9 @@
+﻿namespace CarSales.Core.Contracts
+{
+    public interface ISalesmanService
+    {
+        Task CreateOrRenewSalesmanAsync(string userId);
+
+        Task RetireSalesmanAsync(string userId);
+    }
+}
---
+++ CarSales.Core/Services/SalesmanService.cs
@@ -0,0 +1,48 @@
+﻿using AirsoftMatchMaker.Infrastructure.Data.Common.Repository;
+using CarSales.Core.Contracts;
+using CarSales.Infrastructure.Data.Entities;
+using CarSales.Infrastructure.Data.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarSales.Core.Services
+{
+    public class SalesmanService : ISalesmanService
+    {
+        private readonly IRepository repository;
+        public SalesmanService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+        public async Task CreateOrRenewSalesmanAsync(string userId)
+        {
+            var salesman = await repository.All<Salesman>()
+                .FirstOrDefaultAsync(s => s.UserId == userId);
+            if (salesman != null)
+            {
+                salesman.IsActive = true;
+            }
+            else
+            {
+                salesman = new Salesman()
+                {
+                    UserId = userId,
+                    SalesmanRating = SalesmanRating.Average
+                };
+                await repository.AddAsync<Salesman>(salesman);
+            }
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task RetireSalesmanAsync(string userId)
+        {
+            var salesman = await repository.All<Salesman>()
+                .FirstOrDefaultAsync(s => s.UserId == userId);
+
+            if (salesman != null)
+            {
+                salesman.IsActive = false;
+                await repository.SaveChangesAsync();
+            }
+        }
+    }
+}
