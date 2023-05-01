90614f0ff7569e73d3e3a93c195763199d18a0ba
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-05-01 20:45:51 +0300

    Added model for selling vehicles.

---
+++ CarSales.Core/Models/Vehicles/VehicleSellModel.cs
@@ -0,0 +1,20 @@
+﻿using CarSales.Infrastructure.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarSales.Core.Models.Vehicles
+{
+    public class VehicleSellModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [MinLength(5), MaxLength(100)]
+        public string Description { get; set; } = null!;
+        public string Name { get; set; } = null!;
+
+        public decimal Price { get; set; }
+        public decimal OldPrice { get; set; }
+        public VehicleRating VehicleRating { get; set; }
+        public VehicleRating[] VehicleRatings { get; set; }
+        public string? OwnerUserId { get; set; }
+    }
+}
