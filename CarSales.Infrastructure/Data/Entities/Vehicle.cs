e29e385e5a3cbd353197faaf9435eff20e48c06c
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-05-01 20:46:19 +0300

    Added min/max length for description

--- CarSales.Infrastructure/Data/Entities/Vehicle.cs
+++ CarSales.Infrastructure/Data/Entities/Vehicle.cs
@@ -18,6 +18,7 @@ namespace CarSales.Infrastructure.Data.Entities
         [Required]
         public string Model { get; set; } = null!;
         [Required]
+        [MinLength(5), MaxLength(100)]
         public string Description { get; set; } = null!;
         public string? ImageUrl { get; set; }
         public int YearProduced { get; set; }
