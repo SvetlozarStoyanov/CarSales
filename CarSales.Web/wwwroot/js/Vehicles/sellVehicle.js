722966830e19640a7732da62744a0fdded560fbb
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-05-04 21:36:43 +0300

    Added script which disables the sell button if new price is greater than old price

---
+++ CarSales.Web/wwwroot/js/Vehicles/sellVehicle.js
@@ -0,0 +1,15 @@
+﻿const priceValidationSpan = document.querySelector('#priceValidation');
+
+const newPrice = document.querySelector('#price');
+const oldPrice = document.querySelector('#oldPrice');
+const submitBtn = document.querySelector('#submitBtn');
+
+newPrice.addEventListener('change', function () {
+    if (parseFloat(newPrice.value) > parseFloat(oldPrice.value)) {
+        submitBtn.disabled = true;
+        priceValidationSpan.textContent = `Price cannot be more than the original price (${oldPrice.value})!`;
+    } else {
+        submitBtn.disabled = false;
+        priceValidationSpan.textContent = '';
+    }
+});
