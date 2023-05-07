a95d42b9420bdf4819dff8f43d360efc378f9423
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-05-07 16:09:58 +0300

    Added service for adding or retiring importers

---
+++ CarSales.Core/Contracts/IImporterService.cs
@@ -0,0 +1,28 @@
+﻿using CarSales.Infrastructure.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSales.Core.Contracts
+{
+    public interface IImporterService
+    {
+        /// <summary>
+        /// Creates a new <see cref="Importer"/> with given <paramref name="userId"/>,
+        /// If <see cref="Salesman"/> already exists ot sets <see cref="Importer.IsActive"/> to true
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        Task CreateOrRenewImporterAsync(string userId);
+
+
+        /// <summary>
+        /// Sets <see cref="Importer.IsActive"/> to false
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        Task RetireImporterAsync(string userId);
+    }
+}
---
+++ CarSales.Core/Services/ImporterService.cs
@@ -0,0 +1,53 @@
+﻿using AirsoftMatchMaker.Infrastructure.Data.Common.Repository;
+using CarSales.Core.Contracts;
+using CarSales.Infrastructure.Data.Entities;
+using CarSales.Infrastructure.Data.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSales.Core.Services
+{
+    public class ImporterService : IImporterService
+    {
+        private readonly IRepository repository;
+        public ImporterService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task CreateOrRenewImporterAsync(string userId)
+        {
+            var importer = await repository.All<Importer>()
+                .Where(i => i.UserId == userId)
+                .FirstOrDefaultAsync();
+
+            if (importer == null)
+            {
+                importer = new Importer()
+                {
+                    UserId = userId,
+                    ImporterRating = ImporterRating.Average,
+                };
+                await repository.AddAsync<Importer>(importer);
+            }
+            else
+            {
+                importer.IsActive = true;
+            }
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task RetireImporterAsync(string userId)
+        {
+            var importer = await repository.All<Importer>()
+                .Where(i => i.UserId == userId)
+                .FirstOrDefaultAsync();
+            importer.IsActive = false;
+            await repository.SaveChangesAsync();
+        }
+    }
+}
