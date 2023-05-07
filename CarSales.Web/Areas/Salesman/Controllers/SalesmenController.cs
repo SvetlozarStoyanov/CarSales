8006ee3cf24417935759060c8b22a6c0967e5534
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-05-07 16:24:31 +0300

    Added salesman controller

---
+++ CarSales.Web/Areas/Salesman/Controllers/SalesmenController.cs
@@ -0,0 +1,21 @@
+﻿using CarSales.Core.Contracts;
+using CarSales.Web.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSales.Web.Areas.Salesman.Controllers
+{
+    public class SalesmenController : BaseController
+    {
+        private readonly ISalesmanService salesmanService;
+        public SalesmenController(ISalesmanService salesmanService)
+        {
+            this.salesmanService = salesmanService;
+        }
+
+        public async Task<IActionResult> LeaveSalesmanRole()
+        {
+            await salesmanService.RetireSalesmanAsync(User.Id());
+            return RedirectToAction("LogoutAndLogin", "Users", new { area = "" });
+        }
+    }
+}
