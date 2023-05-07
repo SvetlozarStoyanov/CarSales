187d12dfbbde7386b6327a5c0c9c9474c07b1fe8
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-05-07 16:23:10 +0300

    Added controller for granting importer role

---
+++ CarSales.Web/Areas/Admin/Controllers/ImportersController.cs
@@ -0,0 +1,29 @@
+﻿using CarSales.Core.Contracts;
+using CarSales.Core.Models.RoleRequests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSales.Web.Areas.Admin.Controllers
+{
+    public class ImportersController : BaseController
+    {
+        private readonly IImporterService importerService;
+
+        public ImportersController(IImporterService importerService)
+        {
+            this.importerService = importerService;
+        }
+
+        public async Task<IActionResult> GrantImporterRole(string userId, int roleRequestId)
+        {
+            await importerService.CreateOrRenewImporterAsync(userId);
+
+            var model = new RoleRequestRouteModel()
+            {
+                UserId = userId,
+                RoleName = "Importer",
+                RoleRequestId = roleRequestId.ToString()
+            };
+            return RedirectToAction("GrantRole", "RoleRequests", model);
+        }
+    }
+}
