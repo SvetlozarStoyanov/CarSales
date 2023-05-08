27a4d09fc8b29fa25821ea2ad335dc81acb53ba8
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-05-08 15:56:05 +0300

    Added js files for validating if user has enough credits to buy/import vehicle

---
+++ CarSales.Web/wwwroot/js/Vehicles/canBuyVehicle.js
@@ -0,0 +1,11 @@
+﻿const userCredits = document.querySelector('#userCredits');
+
+const price = document.querySelector('#price');
+
+const buyBtn = document.querySelector('#buyBtn');
+
+if (parseFloat(userCredits.textContent) >= parseFloat(price.textContent)) {
+    buyBtn.disabled = false;
+} else {
+    buyBtn.disabled = true;
+}
---
+++ CarSales.Web/wwwroot/js/Vehicles/canImportVehicle.js
@@ -0,0 +1,15 @@
+﻿const userCredits = document.querySelector('#userCredits');
+
+const price = document.querySelector('#price');
+
+const submitBtn = document.querySelector('#submitBtn');
+
+price.addEventListener('change', function () {
+    let priceCalc = parseFloat(price.value) * 0.9;
+    console.log(priceCalc);
+    if (parseFloat(userCredits.textContent) >= priceCalc) {
+        submitBtn.disabled = false;
+    } else {
+        submitBtn.disabled = true;
+    }
+});
