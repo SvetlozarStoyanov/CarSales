6ca7d098431c614cdf92c9aa2378fe973e1b3857
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-05-11 19:48:11 +0300

    Renamed VehicleListViewModel to VehicleListModel

--- CarSales.Core/Models/Vehicles/VehicleListModel.cs
+++ CarSales.Core/Models/Vehicles/VehicleListModel.cs
@@ -2,7 +2,7 @@
 
 namespace CarSales.Core.Models.Vehicles
 {
-    public class VehicleListViewModel
+    public class VehicleListModel
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
@@ -10,6 +10,9 @@ namespace CarSales.Core.Models.Vehicles
         public VehicleType VehicleType { get; set; }
         public VehicleRating VehicleRating { get; set; }
         public decimal Price { get; set; }
+        public int? OwnerId { get; set; }
+        public string? OwnerName { get; set; }
+        public string? OwnerUserId { get; set; }
         public int? SalesmanId { get; set; }
         public string? SalesmanName { get; set; }
         public string? SalesmanUserId { get; set; }
