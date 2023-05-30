b680a8478084e210074430efa3d69b7a26021b4b
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-05-30 15:56:29 +0300

    Added model and partial view for vehicles. Contains less information that VehicleViewModel

---
+++ CarSales.Core/Models/Vehicles/VehicleMinModel.cs
@@ -0,0 +1,19 @@
+﻿using CarSales.Infrastructure.Data.Enums;
+
+namespace CarSales.Core.Models.Vehicles
+{
+    public class VehicleMinModel
+    {
+        public string Name { get; set; } = null!;
+        public string? ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public VehicleType VehicleType { get; set; }
+        public VehicleRating VehicleRating { get; set; }
+        public int? OwnerId { get; set; }
+        public string? OwnerName { get; set; }
+        public int? SalesmanId { get; set; }
+        public string? SalesmanName { get; set; }
+        public int? ImporterId { get; set; }
+        public string? ImporterName { get; set; }
+    }
+}
