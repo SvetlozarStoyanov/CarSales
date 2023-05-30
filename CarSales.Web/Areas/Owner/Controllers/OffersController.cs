0be84c56116ce8e5fb3ecc68904ce353a7bc6e33
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-05-30 15:54:27 +0300

    Added OffersController and views

---
+++ CarSales.Web/Areas/Owner/Controllers/OffersController.cs
@@ -0,0 +1,58 @@
+﻿using CarSales.Core.Contracts;
+using CarSales.Core.Models.Offers;
+using CarSales.Web.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSales.Web.Areas.Owner.Controllers
+{
+    public class OffersController : BaseController
+    {
+        private readonly IOfferService offerService;
+        public OffersController(IOfferService offerService)
+        {
+            this.offerService = offerService;
+        }
+
+        public async Task<IActionResult> Outgoing()
+        {
+            var model = await offerService.GetOwnerOffersAsync(User.Id());
+            return View(model);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            if (!await offerService.CanViewOfferAsync(User.Id(), id))
+            {
+                return RedirectToAction(nameof(Outgoing));
+            }
+            var model = await offerService.GetOfferByIdAsync(id);
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create(int vehicleId)
+        {
+            var model = await offerService.CreateOfferCreateModelAsync(User.Id(), vehicleId);
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(OfferCreateModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await offerService.CreateOfferAsync(model);
+            TempData["success"] = "Succesfully created offer";
+            return RedirectToAction("Index", "Vehicles");
+        }
+
+        public async Task<IActionResult> Cancel(int id)
+        {
+            await offerService.CancelOfferAsync(id);
+            return RedirectToAction(nameof(Outgoing));
+        }
+    }
+}
---
+++ CarSales.Web/Areas/Salesman/Controllers/OffersController.cs
@@ -0,0 +1,77 @@
+﻿using CarSales.Core.Contracts;
+using CarSales.Core.Models.Offers;
+using CarSales.Web.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSales.Web.Areas.Salesman.Controllers
+{
+    public class OffersController : BaseController
+    {
+        private readonly IOfferService offerService;
+        public OffersController(IOfferService offerService)
+        {
+            this.offerService = offerService;
+        }
+        public async Task<IActionResult> Outgoing()
+        {
+            var model = await offerService.GetOwnerOffersAsync(User.Id());
+            return View(model);
+        }
+
+        public async Task<IActionResult> Incoming()
+        {
+            var model = await offerService.GetSalesmanOffersAsync(User.Id());
+            return View(model);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            if (!await offerService.CanViewOfferAsync(User.Id(),id))
+            {
+                return RedirectToAction(nameof(Outgoing));
+            }
+            var model = await offerService.GetOfferByIdAsync(id);
+            ViewBag.CanRespondToOffer = await offerService.CanRespondToOfferAsync(User.Id(), id);
+            
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create(int vehicleId)
+        {
+            var model = await offerService.CreateOfferCreateModelAsync(User.Id(), vehicleId);
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(OfferCreateModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return RedirectToAction("Index", "Vehicles");
+        }
+
+        public async Task<IActionResult> Cancel(int id)
+        {
+            await offerService.CancelOfferAsync(id);
+            return RedirectToAction(nameof(Outgoing));
+        }
+
+        public async Task<IActionResult> Accept(int id)
+        {
+            await offerService.AcceptOfferAsync(id);
+            TempData["success"] = "Offer accepted!";
+            return RedirectToAction(nameof(Incoming));
+        }
+
+        
+        public async Task<IActionResult> Decline(int id)
+        {
+            await offerService.DeclineOfferAsync(id);
+            TempData["success"] = "Offer declined!";
+            return RedirectToAction(nameof(Incoming));
+        }
+    }
+}
