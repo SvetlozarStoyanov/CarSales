982d404972271cb31587e5331c9cfc575ab6c8d5
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-05-30 15:54:59 +0300

    Added script for creating offers

---
+++ CarSales.Web/wwwroot/js/Offers/canCreateOffer.js
@@ -0,0 +1,14 @@
+﻿const userCredits = parseFloat(document.querySelector('#userCredits').textContent);
+
+const price = document.querySelector('#price');
+
+const submitBtn = document.querySelector('#submitBtn');
+
+price.addEventListener('change', function () {
+    let priceParsed = parseFloat(price.value);
+    if (userCredits >= priceParsed && priceParsed >= 0) {
+        submitBtn.disabled = false;
+    } else {
+        submitBtn.disabled = true;
+    }
+});
