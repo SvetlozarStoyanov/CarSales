388375b94c37542e721ebd461116462dbc65bde8
SvetlozarStoyanov <svetlio214@gmail.com>
2023-06-02 15:11:01 +0300

    New model for editing offers

---
+++ CarSales.Core/Models/Offers/OfferEditModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CarSales.Core.Models.Offers
+{
+    public class OfferEditModel
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal OldPrice { get; set; }
+        public decimal AvailableCredits { get; set; }
+    }
+}
