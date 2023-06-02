0a2f1f49e84328e5193074b76170574f233d94b4
SvetlozarStoyanov <svetlio214@gmail.com>
2023-06-02 15:11:14 +0300

    Added available credits property

--- CarSales.Core/Models/Offers/OfferCreateModel.cs
+++ CarSales.Core/Models/Offers/OfferCreateModel.cs
@@ -1,12 +1,11 @@
-﻿using CarSales.Infrastructure.Data.Entities;
-
-namespace CarSales.Core.Models.Offers
+﻿namespace CarSales.Core.Models.Offers
 {
     public class OfferCreateModel
     {
         public string? Description { get; set; }
         public decimal Price { get; set; }
         public decimal InitialPrice { get; set; }
+        public decimal AvailableCredits { get; set; }
         public int VehicleId { get; set; }
         public int OfferorId { get; set; }
         public int SalesmanId { get; set; }
