c7a850a099b60d6d1166ddaf3392af93f34d528f
SvetlozarStoyanov <svetlio214@gmail.com>
2023-06-19 21:09:47 +0300

    Controller for granting reviewer role

---
+++ CarSales.Web/Areas/Admin/Controllers/ReviewersController.cs
@@ -0,0 +1,31 @@
+﻿using CarSales.Core.Contracts;
+using CarSales.Core.Models.RoleRequests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSales.Web.Areas.Admin.Controllers
+{
+    public class ReviewersController : BaseController
+    {
+        private readonly IReviewerService reviewerService;
+
+        public ReviewersController(IReviewerService salesmanService)
+        {
+            this.reviewerService = salesmanService;
+        }
+
+        public async Task<IActionResult> GrantReviewerRole(string userId, int roleRequestId)
+        {
+            await reviewerService.CreateOrRenewReviewerAsync(userId);
+
+            var model = new RoleRequestRouteModel()
+            {
+                UserId = userId,
+                RoleName = "Reviewer",
+                RoleRequestId = roleRequestId.ToString()
+            };
+            return RedirectToAction("GrantRole", "RoleRequests", model);
+        }
+
+
+    }
+}
