9069ecd2e9aad6609414f112f1f67604410235bf
SvetlozarStoyanov <svetlio214@gmail.com>
2023-06-20 22:12:28 +0300

    Added reviewer controllers.

---
+++ CarSales.Web/Areas/Reviewer/Controllers/ReviewersController.cs
@@ -0,0 +1,35 @@
+﻿using CarSales.Core.Contracts;
+using CarSales.Core.Models.Reviewers;
+using CarSales.Web.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSales.Web.Areas.Reviewer.Controllers
+{
+    public class ReviewersController : BaseController
+    {
+        private readonly IReviewerService reviewerService;
+
+        public ReviewersController(IReviewerService reviewerService)
+        {
+            this.reviewerService = reviewerService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditReviewPrices()
+        {
+            var model = await reviewerService.CreateReviewerPriceEditModelAsync(User.Id());
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditReviewPrices(ReviewerPriceEditModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            await reviewerService.EditReviewPricesAsync(model);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ CarSales.Web/Areas/Salesman/Controllers/ReviewersController.cs
@@ -0,0 +1,29 @@
+﻿using CarSales.Core.Contracts;
+using CarSales.Core.Models.Reviewers;
+using CarSales.Web.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSales.Web.Areas.Salesman.Controllers
+{
+    public class ReviewersController : BaseController
+    {
+        private readonly IReviewerService reviewerService;
+
+        public ReviewersController(IReviewerService reviewerService)
+        {
+            this.reviewerService = reviewerService;
+        }
+
+        public async Task<IActionResult> Index(int vehicleId, [FromQuery] ReviewersQueryModel model)
+        {
+            var queryResult = await reviewerService.GetAllReviewersAsync(User.Id(),
+                vehicleId,
+                model.SearchTerm);
+
+            model.SearchTerm = queryResult.SearchTerm;
+            model.VehicleId = vehicleId;
+            model.Reviewers = queryResult.Reviewers;
+            return View(model);
+        }
+    }
+}
