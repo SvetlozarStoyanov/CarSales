63c6b4a5159058f6da85574f2b41ec925f6594f3
SvetlozarStoyanov <svetlio214@gmail.com>
2023-06-20 22:11:18 +0300

    Added new review controllers and views

---
+++ CarSales.Web/Areas/Salesman/Controllers/ReviewsController.cs
@@ -0,0 +1,35 @@
+﻿using CarSales.Core.Contracts;
+using CarSales.Core.Models.Reviews;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSales.Web.Areas.Salesman.Controllers
+{
+    public class ReviewsController : BaseController
+    {
+        private readonly IReviewService reviewService;
+        public ReviewsController(IReviewService reviewService)
+        {
+            this.reviewService = reviewService;
+        }
+
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> OrderReview(int reviewerId, int vehicleId)
+        {
+            var model = await reviewService.CreateReviewOrderModelAsync(reviewerId, vehicleId);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> OrderReview(ReviewOrderModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("Details", "Vehicles", new { id = model.VehicleId });
+        }
+    }
+}
---
+++ CarSales.Web/Controllers/ReviewsController.cs
@@ -0,0 +1,26 @@
+﻿using CarSales.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSales.Web.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private readonly IReviewService reviewService;
+
+        public ReviewsController(IReviewService reviewService)
+        {
+            this.reviewService = reviewService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var model = await reviewService.GetReviewByIdAsync(id);
+            return View(model);
+        }
+    }
+}
