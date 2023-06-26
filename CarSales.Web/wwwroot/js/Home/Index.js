0933fa54c160969c0e2e9e6542bf80f41dc48818
SvetlozarStoyanov <svetlio214@gmail.com>
2023-06-26 16:58:16 +0300

    Js file for landing page

---
+++ CarSales.Web/wwwroot/js/Home/Index.js
@@ -0,0 +1,35 @@
+﻿const reviewContainerArticles = document.querySelectorAll('.review-container');
+const highlightedReviewContainerArticle = document.querySelector('.review-hightlighted-container');
+
+
+window.addEventListener('load', function () {
+    highlightedReviewContainerArticle.addEventListener('mouseenter', function () {
+        highlightedReviewContainerArticle.classList.add('border-primary');
+        highlightedReviewContainerArticle.classList.remove('border-dark');
+    });
+
+    highlightedReviewContainerArticle.addEventListener('mouseleave', function () {
+        highlightedReviewContainerArticle.classList.remove('border-primary');
+        highlightedReviewContainerArticle.classList.add('border-dark');
+    });
+});
+
+
+for (let i = 0; i < reviewContainerArticles.length; i++) {
+    const currReviewContainer = reviewContainerArticles[i];
+    if (i % 2 !== 0) {
+        currReviewContainer.classList.add('flex-row-reverse');
+    }
+
+    currReviewContainer.addEventListener('mouseenter', function () {
+        currReviewContainer.classList.add('border-primary');
+        currReviewContainer.classList.remove('border-dark');
+    });
+
+    currReviewContainer.addEventListener('mouseleave', function () {
+        currReviewContainer.classList.remove('border-primary');
+        currReviewContainer.classList.add('border-dark');
+    });
+}
+
+
