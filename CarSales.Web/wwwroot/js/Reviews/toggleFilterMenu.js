01febeb06c11f69c1f875cf96448f0fbb685b57f
SvetlozarStoyanov <svetlio214@gmail.com>
2023-06-27 18:48:41 +0300

    Script which changes filter button nested icon class

---
+++ CarSales.Web/wwwroot/js/Reviews/toggleFilterMenu.js
@@ -0,0 +1,9 @@
+﻿const filterBtn = document.querySelector('#filterBtn');
+const filterIcon = filterBtn.querySelector('i');
+
+filterBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    e.currentTarget.blur();
+    filterIcon.classList.toggle('bi-filter-circle');
+    filterIcon.classList.toggle('bi-filter-circle-fill');
+});
