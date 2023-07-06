892d978991903baeb16c9778359f2dc891491f82
SvetlozarStoyanov <svetlio214@gmail.com>
2023-07-06 18:18:26 +0300

    Added script for editing users

---
+++ CarSales.Web/wwwroot/js/Users/myAccount.js
@@ -0,0 +1,24 @@
+﻿const changeButtons = document.querySelectorAll('button[type="button"]');
+const submitBtns = document.querySelectorAll('button[type="submit"]');
+
+
+
+console.log(submitBtns.length);
+for (const btn of changeButtons) {
+    btn.addEventListener('click', function (e) {
+        e.preventDefault();
+        const parentDiv = e.currentTarget.parentElement;
+        const input = parentDiv.querySelector('input');
+        const submitBtn = parentDiv.querySelector('button[type="submit"]');
+        e.currentTarget.classList.add('d-none');
+        input.disabled = false;
+        submitBtn.classList.remove('d-none');
+    });
+}
+
+//for (const btn of submitBtns) {
+//    btn.addEventListener('click', function (e) {
+//        e.preventDefault();
+//        console.log('submit clicked!');
+//    });
+//}
