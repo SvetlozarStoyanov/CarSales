5dbca18863238382dd4c926628aa6d3eccf19d47
SvetlozarStoyanov <svetlio214@gmail.com>
2023-07-06 12:17:19 +0300

    Added name and review count properties

--- CarSales.Core/Models/Vehicles/VehicleViewModel.cs
+++ CarSales.Core/Models/Vehicles/VehicleViewModel.cs
@@ -7,12 +7,14 @@ namespace CarSales.Core.Models.Vehicles
         public int Id { get; set; }
         public string Brand { get; set; } = null!;
         public string Model { get; set; } = null!;
+        public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
         public string? ImageUrl { get; set; }
         public int YearProduced { get; set; }
         public double TopSpeed { get; set; }
         public double KilometersDriven { get; set; }
         public decimal Price { get; set; }
+        public int ReviewCount { get; set; }
         public VehicleType VehicleType { get; set; }
         public VehicleRating VehicleRating { get; set; }
         public int? OwnerId { get; set; }
