348e6fb69a72d61dbd8503dba183aa56352a66c1
SvetlozarStoyanov <svetlio214@gmail.com>
2023-07-06 18:17:52 +0300

    Added controller and view for user viewing/editing

---
+++ CarSales.Web/Areas/Owner/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using CarSales.Core.Contracts;
+using CarSales.Core.Models.Users;
+using CarSales.Web.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSales.Web.Areas.Owner.Controllers
+{
+    public class UsersController : BaseController
+    {
+        private readonly IUserService userService;
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task<IActionResult> MyAccount()
+        {
+            var model = await userService.GetUserByIdAsync(User.Id());
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UserEditModel userEditModel)
+        {
+            if (ModelState.IsValid)
+            {
+                await userService.EditUserAsync(userEditModel);
+            }
+            return RedirectToAction(nameof(MyAccount));
+        }
+    }
+}
