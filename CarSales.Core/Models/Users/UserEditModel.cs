45dbdcf2f0d8b0a78fd493d19e6766bf6edcba02
SvetlozarStoyanov <svetlio214@gmail.com>
2023-07-06 18:17:32 +0300

    New models for viewing/editing users

---
+++ CarSales.Core/Models/Users/UserEditModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSales.Core.Models.Users
+{
+    public class UserEditModel
+    {
+        public string Id { get; set; } = null!;
+        [MinLength(3), MaxLength(30)]
+        public string? UserName { get; set; }
+        [Phone]
+        public string? PhoneNumber { get; set; }
+    }
+}
---
+++ CarSales.Core/Models/Users/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CarSales.Core.Models.Users
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public string? PhoneNumber { get; set; }
+        public decimal Credits { get; set; }
+        public UserEditModel UserEditModel { get; set; }
+    }
+}
