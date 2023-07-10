828cd15b2f534a657dfa0851692a5ee5e90ce3c0
SvetlozarStoyanov <svetlio214@gmail.com>
2023-07-10 19:26:01 +0300

    Added first name, last name, email , image url and credits properties

--- CarSales.Core/Models/Users/UserEditModel.cs
+++ CarSales.Core/Models/Users/UserEditModel.cs
@@ -6,8 +6,17 @@ namespace CarSales.Core.Models.Users
     {
         public string Id { get; set; } = null!;
         [MinLength(3), MaxLength(30)]
-        public string? UserName { get; set; }
+        public string FirstName { get; set; } = null!;
+        [MinLength(3), MaxLength(30)]
+        public string LastName { get; set; } = null!;
+        [MinLength(3), MaxLength(30)]
+        public string UserName { get; set; } = null!;
+        [EmailAddress]
+        public string Email { get; set; } = null!;
         [Phone]
         public string? PhoneNumber { get; set; }
+        [Url]
+        public string? ImageUrl { get; set; }
+        public decimal? Credits { get; set; }
     }
 }
