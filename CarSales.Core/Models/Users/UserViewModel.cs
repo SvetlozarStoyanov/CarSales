69ec72a0401dee6a967c100e77ecdef81b888e0d
SvetlozarStoyanov <svetlio214@gmail.com>
2023-07-10 19:25:14 +0300

    Added email property

--- CarSales.Core/Models/Users/UserViewModel.cs
+++ CarSales.Core/Models/Users/UserViewModel.cs
@@ -6,10 +6,11 @@
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
         public string UserName { get; set; } = null!;
+        public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? ImageUrl { get; set; }
         public string? Gender { get; set; }
         public decimal Credits { get; set; }
-        public UserEditModel UserEditModel { get; set; }
+        public UserEditModel UserEditModel { get; set; } = null!;
     }
 }
