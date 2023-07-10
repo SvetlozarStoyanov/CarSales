caf36e3074af834be916ef4beb89740bdb2a2683
SvetlozarStoyanov <svetlio214@gmail.com>
2023-07-10 19:28:47 +0300

    Script for MyAccount page now shows image preview when changing user image . Enables save changes button whenever inputs get changed

--- CarSales.Web/wwwroot/js/Users/myAccount.js
+++ CarSales.Web/wwwroot/js/Users/myAccount.js
@@ -1,19 +1,23 @@
-﻿const changeButtons = document.querySelectorAll('button[type="button"]');
-const submitBtns = document.querySelectorAll('button[type="submit"]');
+﻿const formInputs = document.querySelectorAll('.form-control');
+const submitBtn = document.querySelector('button[type="submit"]');
+const profilePicture = document.querySelector('#profilePicture');
+const defaultProfilePictureSrc = document.querySelector('#defaultProfilePicture');
+const urlRegex = new RegExp("(www|http:|https:)+[^\s]+[\w]");
 
+console.log(formInputs.length);
+for (const input of formInputs) {
+    input.addEventListener('input', function (e) {
+        submitBtn.disabled = false;
+        if (e.currentTarget.id === 'imageUrl') {
+            let imageUrlInput = e.currentTarget;
+            if (imageUrlInput.value.match(urlRegex)) {
+                profilePicture.src = e.currentTarget.value;
+            } else {
+                profilePicture.src = defaultProfilePictureSrc.value;
+            }
 
-
-console.log(submitBtns.length);
-for (const btn of changeButtons) {
-    btn.addEventListener('click', function (e) {
-        e.preventDefault();
-        const parentDiv = e.currentTarget.parentElement;
-        const input = parentDiv.querySelector('input');
-        const submitBtn = parentDiv.querySelector('button[type="submit"]');
-        e.currentTarget.classList.add('d-none');
-        input.disabled = false;
-        submitBtn.classList.remove('d-none');
-    });
+        }
+    })
 }
 
 //for (const btn of submitBtns) {
