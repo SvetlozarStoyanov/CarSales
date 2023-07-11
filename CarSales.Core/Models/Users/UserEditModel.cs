d42a2eaf1036b7a5c1820e4f953bdda28798af23
SvetlozarStoyanov <svetlio214@gmail.com>
2023-07-11 20:35:56 +0300

    Removed credits from model. Added error messages for fields

--- CarSales.Core/Models/Users/UserEditModel.cs
+++ CarSales.Core/Models/Users/UserEditModel.cs
@@ -1,22 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CarSales.Infrastructure.Data.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarSales.Core.Models.Users
 {
     public class UserEditModel
     {
         public string Id { get; set; } = null!;
-        [MinLength(3), MaxLength(30)]
+        [Required]
+        [MinLength(3, ErrorMessage = "First name must be at least 3 characters long"), MaxLength(30, ErrorMessage = "First name must be less than 30 characters long")]
         public string FirstName { get; set; } = null!;
-        [MinLength(3), MaxLength(30)]
+        [Required]
+        [MinLength(3, ErrorMessage = "Last name Must be at least 3 characters long"), MaxLength(30, ErrorMessage = "Last name must be less than 30 characters long")]
         public string LastName { get; set; } = null!;
-        [MinLength(3), MaxLength(30)]
+        [Required]
+        [MinLength(3, ErrorMessage = "Username be at least 3 characters long"), MaxLength(30, ErrorMessage = "Username must be less than 30 characters long")]
         public string UserName { get; set; } = null!;
+        [Required]
         [EmailAddress]
         public string Email { get; set; } = null!;
         [Phone]
         public string? PhoneNumber { get; set; }
+        public Gender Gender { get; set; }
         [Url]
         public string? ImageUrl { get; set; }
-        public decimal? Credits { get; set; }
     }
 }
