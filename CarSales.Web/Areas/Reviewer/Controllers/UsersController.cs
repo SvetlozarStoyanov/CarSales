ecdd6bdb1dc9eec23b24381fd76177cfb4284ecd
SvetlozarStoyanov <svetlio214@gmail.com>
2023-07-12 19:14:34 +0300

    Added user controllers to all areas

---
+++ CarSales.Web/Areas/Reviewer/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using CarSales.Core.Contracts;
+using CarSales.Core.Models.Users;
+using CarSales.Web.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSales.Web.Areas.Reviewer.Controllers
+{
+    public class UsersController : BaseController
+    {
+        private readonly IUserService userService;
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            var model = await userService.GetUserByIdAsync(id);
+            ViewBag.CanEditProfile = await userService.CanEditProfileAsync(id, User.Id());
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            var model = await userService.CreateUserEditModelAsync(id);
+            var canEditProfile = await userService.CanEditProfileAsync(id, User.Id());
+            if (!canEditProfile)
+            {
+                TempData["error"] = "Can only edit own profile!";
+                return RedirectToAction(nameof(Details), new { id = id });
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UserEditModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            await userService.EditUserAsync(model);
+            return RedirectToAction(nameof(Details), new { id = model.Id });
+        }
+    }
+}
