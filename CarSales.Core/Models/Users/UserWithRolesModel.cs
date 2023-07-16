84cee137f389af3b5b2b47a672b7df5667a3c45d
SvetlozarStoyanov <svetlio214@gmail.com>
2023-07-16 22:32:01 +0300

    Added model for which contains user information along with his roles

---
+++ CarSales.Core/Models/Users/UserWithRolesModel.cs
@@ -0,0 +1,20 @@
+﻿using CarSales.Infrastructure.Data.Enums;
+
+namespace CarSales.Core.Models.Users
+{
+    public class UserWithRolesModel
+    {
+        public UserWithRolesModel()
+        {
+            Roles = new HashSet<string>();
+        }
+        public string Id { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public string? Email { get; set; }
+        public string? ImageUrl { get; set; }
+        public Gender Gender { get; set; }
+        public ICollection<string> Roles { get; set; }
+    }
+}
