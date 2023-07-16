878c5a1e3341e677928effe18572712431afaf3c
SvetlozarStoyanov <svetlio214@gmail.com>
2023-07-16 22:32:16 +0300

    Added UserWithRolesModel

--- CarSales.Core/Models/RoleRequests/RoleRequestViewModel.cs
+++ CarSales.Core/Models/RoleRequests/RoleRequestViewModel.cs
@@ -1,12 +1,12 @@
-﻿namespace CarSales.Core.Models.RoleRequests
+﻿using CarSales.Core.Models.Users;
+
+namespace CarSales.Core.Models.RoleRequests
 {
     public class RoleRequestViewModel
     {
         public int Id { get; set; }
         public string RoleId { get; set; }
         public string RoleName { get; set; }
-        public string UserId { get; set; }
-        public string UserName { get; set; }
-        public ICollection<string> UserRoles { get; set; }
+        public UserWithRolesModel UserModel { get; set; } = null!;
     }
 }
