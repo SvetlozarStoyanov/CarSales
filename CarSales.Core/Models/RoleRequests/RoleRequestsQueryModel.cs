65c054be856815ebff6191f83618b5237cfc8aba
SvetlozarStoyanov <svetlio214@gmail.com>
2023-07-17 23:20:55 +0300

    New query model for role requests

---
+++ CarSales.Core/Models/RoleRequests/RoleRequestsQueryModel.cs
@@ -0,0 +1,30 @@
+﻿using CarSales.Core.Enums;
+
+namespace CarSales.Core.Models.RoleRequests
+{
+    public class RoleRequestsQueryModel
+    {
+        public RoleRequestsQueryModel()
+        {
+            UserNames = new HashSet<string>();
+            RoleNames = new HashSet<string>();
+            SortingOptions = Enum.GetValues<RoleRequestSorting>().ToHashSet();
+            RoleRequests = new List<RoleRequestListModel>();
+        }
+
+        public string? SearchTerm { get; set; }
+        public int CurrentPage { get; set; } = 1;
+        public int RoleRequestsPerPage { get; set; } = 12;
+        public int FirstPage { get; set; } = 1;
+        public int MaxPage { get; set; }
+        public string? SelectedUserName { get; set; }
+        public string? SelectedRoleNames { get; set; }
+        public RoleRequestSorting Sorting { get; set; }
+        public ICollection<string> UserNames { get; set; }
+        public ICollection<string> RoleNames { get; set; }
+        public IEnumerable<int> PreviousPages { get; set; }
+        public IEnumerable<int> NextPages { get; set; }
+        public ICollection<RoleRequestSorting> SortingOptions { get; set; }
+        public IEnumerable<RoleRequestListModel> RoleRequests { get; set; }
+    }
+}
