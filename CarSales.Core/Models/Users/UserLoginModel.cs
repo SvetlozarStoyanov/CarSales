3406cf96a1acedabca29b3fe64f5eccb6c672607
SvetlozarStoyanov <svetlio214@gmail.com>
2023-07-22 16:35:45 +0300

    Added Required attributes to all properties

--- CarSales.Core/Models/Users/UserLoginModel.cs
+++ CarSales.Core/Models/Users/UserLoginModel.cs
@@ -4,7 +4,9 @@ namespace CarSales.Core.Models.Users
 {
     public class UserLoginModel
     {
+        [Required(ErrorMessage = "Required field")]
         public string UserName { get; set; }
+        [Required(ErrorMessage = "Required field")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
