d6f7e4259314e2b413d10037d4ad6d6079836958
SvetlozarStoyanov <svetlio214@gmail.com>
2023-07-24 17:44:43 +0300

    New QueryModel for sorting/filtering offers

---
+++ CarSales.Core/Models/Offers/OffersQueryModel.cs
@@ -0,0 +1,26 @@
+﻿using CarSales.Core.Enums;
+
+namespace CarSales.Core.Models.Offers
+{
+    public class OffersQueryModel
+    {
+        public OffersQueryModel()
+        {
+            VehicleNames = new HashSet<string>();
+            SortingOptions = Enum.GetValues<OfferSorting>().ToHashSet();
+            Offers = new List<OfferListModel>();
+        }
+        public int CurrentPage { get; set; } = 1;
+        public int OffersPerPage { get; set; } = 12;
+        public int FirstPage { get; set; } = 1;
+        public int MaxPage { get; set; }
+        public int OfferCount { get; set; }
+        public string? VehicleName { get; set; }
+        public OfferSorting OfferSorting { get; set; }
+        public ICollection<string> VehicleNames { get; set; }
+        public IEnumerable<int> PreviousPages { get; set; }
+        public IEnumerable<int> NextPages { get; set; }
+        public ICollection<OfferSorting> SortingOptions { get; set; }
+        public IEnumerable<OfferListModel> Offers { get; set; }
+    }
+}
