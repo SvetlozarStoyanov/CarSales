e78471f4c715ab0d481a5c4cf77cbce58058a1a6
SvetlozarStoyanov <svetlio214@gmail.com>
2023-07-25 15:38:00 +0300

    Added properties for SalesmanName, OfferorName , SalesmanNames. OfferorNames

--- CarSales.Core/Models/Offers/OffersQueryModel.cs
+++ CarSales.Core/Models/Offers/OffersQueryModel.cs
@@ -11,13 +11,17 @@ namespace CarSales.Core.Models.Offers
             Offers = new List<OfferListModel>();
         }
         public int CurrentPage { get; set; } = 1;
-        public int OffersPerPage { get; set; } = 12;
+        public int OffersPerPage { get; set; } = 6;
         public int FirstPage { get; set; } = 1;
         public int MaxPage { get; set; }
         public int OfferCount { get; set; }
         public string? VehicleName { get; set; }
+        public string? SalesmanName { get; set; }
+        public string? OfferorName { get; set; }
         public OfferSorting OfferSorting { get; set; }
         public ICollection<string> VehicleNames { get; set; }
+        public ICollection<string> SalesmenNames { get; set; }
+        public ICollection<string> OfferorNames { get; set; }
         public IEnumerable<int> PreviousPages { get; set; }
         public IEnumerable<int> NextPages { get; set; }
         public ICollection<OfferSorting> SortingOptions { get; set; }
