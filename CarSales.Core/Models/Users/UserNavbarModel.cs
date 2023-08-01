f905950aea8a78185f35a9da762132854322363a
SvetlozarStoyanov <svetlio214@gmail.com>
2023-08-01 17:07:01 +0300

    Added user model for the navbar.

---
+++ CarSales.Core/Models/Users/UserNavbarModel.cs
@@ -0,0 +1,12 @@
+﻿using CarSales.Infrastructure.Data.Enums;
+
+namespace CarSales.Core.Models.Users
+{
+    public class UserNavbarModel
+    {
+        public string Id { get; set; } = null!;
+        public Gender Gender { get; set; }
+        public string ProfilePicture { get; set; } = null!;
+        public decimal Credits { get; set; }
+    }
+}
