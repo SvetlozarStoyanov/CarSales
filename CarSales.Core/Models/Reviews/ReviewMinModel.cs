b9dbf9ab1b8c7be59be40264601c8529929243f2
SvetlozarStoyanov <svetlio214@gmail.com>
2023-08-08 14:51:25 +0300

    Added new review min model

---
+++ CarSales.Core/Models/Reviews/ReviewMinModel.cs
@@ -0,0 +1,12 @@
+﻿using CarSales.Infrastructure.Data.Enums;
+
+namespace CarSales.Core.Models.Reviews
+{
+    public class ReviewMinModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string ReviewerName { get; set; } = null!;
+        public VehicleRating VehicleRating { get; set; }
+    }
+}
