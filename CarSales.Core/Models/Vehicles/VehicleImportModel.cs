6ab8435accec32f4d3e6d8deab0f2bd6946453de
SvetlozarStoyanov <svetlio214@gmail.com>
2023-08-12 11:29:24 +0300

    Added validation attributes to properties

--- CarSales.Core/Models/Vehicles/VehicleImportModel.cs
+++ CarSales.Core/Models/Vehicles/VehicleImportModel.cs
@@ -5,18 +5,21 @@ namespace CarSales.Core.Models.Vehicles
 {
     public class VehicleImportModel
     {
-        [Required]
+        [Required(ErrorMessage = "Required field")]
         public string Brand { get; set; } = null!;
-        [Required]
+        [Required(ErrorMessage = "Required field")]
         public string ModelName { get; set; } = null!;
-        [Required]
-        [MinLength(5), MaxLength(100)]
+        [Required(ErrorMessage = "Required field")]
+        [MinLength(5,ErrorMessage = "Must be more than 5 symbols"), MaxLength(100, ErrorMessage = "Must be less than 100 symbols")]
         public string Description { get; set; } = null!;
         public string? ImageUrl { get; set; }
         public int YearProduced { get; set; }
+        [Required(ErrorMessage = "Required field")]
         public double TopSpeed { get; set; }
-        public double KilometersDriven { get; set; } = 0;
+        [Required(ErrorMessage = "Required field")]
         public VehicleType VehicleType { get; set; }
+        [Required]
+        [Range(1.00,9900000.00 ,ErrorMessage = "Range 1 - 9900000")]
         public decimal Price { get; set; }
         public int? ImporterId { get; set; }
         public VehicleType[]? VehicleTypes { get; set; }
