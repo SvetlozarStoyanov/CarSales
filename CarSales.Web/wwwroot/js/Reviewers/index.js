add3498bfb77ddd0ea0e03a3af9d13da10a5ee5f
SvetlozarStoyanov <svetlio214@gmail.com>
2023-08-15 20:55:39 +0300

    New js file for Salesman/Reviewers/Index

---
+++ CarSales.Web/wwwroot/js/Reviewers/index.js
@@ -0,0 +1,11 @@
+﻿const filterBtn = document.querySelector('#filterBtn');
+const filterIcon = filterBtn.querySelector('i');
+
+const submitFormBtn = document.querySelector('#submitBtn');
+
+filterBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    e.currentTarget.blur();
+    filterIcon.classList.toggle('bi-filter-circle');
+    filterIcon.classList.toggle('bi-filter-circle-fill');
+});
