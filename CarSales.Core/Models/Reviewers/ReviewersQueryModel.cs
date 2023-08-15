f8e1d06661a0cf889c5d079480f09ad0ba810026
SvetlozarStoyanov <svetlio214@gmail.com>
2023-08-15 20:53:53 +0300

    Added properties to model to improve sorting/filtering ability

--- CarSales.Core/Models/Reviewers/ReviewersQueryModel.cs
+++ CarSales.Core/Models/Reviewers/ReviewersQueryModel.cs
@@ -1,4 +1,4 @@
-﻿using CarSales.Infrastructure.Data.Enums;
+﻿using CarSales.Core.Enums;
 
 namespace CarSales.Core.Models.Reviewers
 {
@@ -7,10 +7,20 @@ namespace CarSales.Core.Models.Reviewers
         public ReviewersQueryModel()
         {
             Reviewers = new List<ReviewerListModel>();
+            SortingOptions = Enum.GetValues<ReviewerSorting>().ToHashSet();
         }
 
+        public int ReviewersPerPage { get; set; } = 6;
         public string? SearchTerm { get; set; }
+        public int CurrentPage { get; set; } = 1;
+        public int FirstPage { get; set; } = 1;
+        public int MaxPage { get; set; }
+        public int ReviewsCount { get; set; }
         public int? VehicleId { get; set; }
+        public ReviewerSorting ReviewerSorting { get; set; }
+        public ICollection<ReviewerSorting> SortingOptions { get; set; }
+        public IEnumerable<int> PreviousPages { get; set; }
+        public IEnumerable<int> NextPages { get; set; }
         public ICollection<ReviewerListModel> Reviewers { get; set; }
     }
 }
