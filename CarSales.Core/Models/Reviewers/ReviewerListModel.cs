172f019adaf6c34e72ccc8271beb34f86fb08938
SvetlozarStoyanov <svetlio214@gmail.com>
2023-08-16 17:03:00 +0300

    Added profile picture property

--- CarSales.Core/Models/Reviewers/ReviewerListModel.cs
+++ CarSales.Core/Models/Reviewers/ReviewerListModel.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
+        public string? ProfilePicture { get; set; }
         public decimal? ShortReviewPrice { get; set; }
         public decimal? StandartReviewPrice { get; set; }
         public decimal? PremiumReviewPrice { get; set; }
