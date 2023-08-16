286bf2e6f42ba69fbebc1aab620d3030eb498043
SvetlozarStoyanov <svetlio214@gmail.com>
2023-08-16 17:04:06 +0300

    Added reviewer ViewModel

---
+++ CarSales.Core/Models/Reviewers/ReviewerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CarSales.Core.Models.Reviews;
+
+namespace CarSales.Core.Models.Reviewers
+{
+    public class ReviewerViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool  CanCreateReview { get; set; }
+        public string? ProfilePicture { get; set; }
+        public string? PhoneNumber { get; set; }
+        public int? VehicleId { get; set; }
+        public decimal? ShortReviewPrice { get; set; }
+        public decimal? StandartReviewPrice { get; set; }
+        public decimal? PremiumReviewPrice { get; set; }
+        public IEnumerable<ReviewListModel> Reviews { get; set; }
+    }
+}
