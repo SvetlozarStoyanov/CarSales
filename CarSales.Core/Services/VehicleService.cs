8ee48d6734e668ad7e440283af55ee88e3113d3b
SvetlozarStoyanov <svetlio214@gmail.com>
2023-08-21 14:27:40 +0300

    Fixed GetVehicleById method showing ordered reviews

--- CarSales.Core/Services/VehicleService.cs
+++ CarSales.Core/Services/VehicleService.cs
@@ -421,7 +421,7 @@ namespace CarSales.Core.Services
                     ImporterId = v.ImporterId,
                     ImporterUserId = v.Importer != null ? v.Importer.UserId : null,
                     ImporterName = v.Importer != null ? $"{v.Importer.User.FirstName} {v.Importer.User.LastName}" : null,
-                    Reviews = v.Reviews.Count > 0 ? v.Reviews.OrderByDescending(r => r.Id).Take(3).Select(r => new ReviewMinModel()
+                    Reviews = v.Reviews.Count > 0 ? v.Reviews.Where(r => r.ReviewStatus == ReviewStatus.Completed).OrderByDescending(r => r.Id).Take(3).Select(r => new ReviewMinModel()
                     {
                         Id = r.Id,
                         Title = r.Title,
