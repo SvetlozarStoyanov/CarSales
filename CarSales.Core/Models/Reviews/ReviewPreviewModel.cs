e5c02e04bb9f9255169ab1d162c34c927515de22
SvetlozarStoyanov <svetlio214@gmail.com>
2023-08-23 15:44:21 +0300

    Added new model for previewing reviews when creating them

---
+++ CarSales.Core/Models/Reviews/ReviewPreviewModel.cs
@@ -0,0 +1,19 @@
+﻿using CarSales.Core.Models.Vehicles;
+using CarSales.Infrastructure.Data.Enums;
+
+namespace CarSales.Core.Models.Reviews
+{
+    public class ReviewPreviewModel
+    {
+        public string? Title { get; set; }
+        public string? Overview { get; set; }
+        public string? Performance { get; set; } 
+        public string? Interior { get; set; } 
+        public string? Longevity { get; set; }
+        public string? Features { get; set; }
+        public VehicleRating VehicleRating { get; set; }
+        public int VehicleRatingAsInt { get; set; }
+        public VehicleReviewModel Vehicle { get; set; }
+
+    }
+}
