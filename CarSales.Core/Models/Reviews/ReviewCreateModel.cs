8c103f5d205a17d985b7bbdc8e39a84f5124e910
SvetlozarStoyanov <svetlio214@gmail.com>
2023-08-23 15:44:51 +0300

    Added new PreviewModel and added Validation Attribute ErrorMessages

--- CarSales.Core/Models/Reviews/ReviewCreateModel.cs
+++ CarSales.Core/Models/Reviews/ReviewCreateModel.cs
@@ -11,21 +11,26 @@ namespace CarSales.Core.Models.Reviews
             VehicleRatings = Enum.GetValues<VehicleRating>().Skip(1).ToHashSet();
         }
         public int Id { get; set; }
-        [MinLength(10), MaxLength(100)]
+        [Required(ErrorMessage = "Required")]
+        [MinLength(10, ErrorMessage = "Must be above 10 symbols"), MaxLength(100, ErrorMessage = "Must be below 100 symbols")]
         public string Title { get; set; } = null!;
-        [MinLength(10), MaxLength(1000)]
+        [Required(ErrorMessage = "Required")]
+        [MinLength(10, ErrorMessage = "Must be above 10 symbols"), MaxLength(1000, ErrorMessage = "Must be below 1000 symbols")]
         public string Overview { get; set; } = null!;
-        [MinLength(10), MaxLength(1000)]
+        [Required(ErrorMessage = "Required")]
+        [MinLength(10, ErrorMessage = "Must be above 10 symbols"), MaxLength(1000, ErrorMessage = "Must be below 1000 symbols")]
         public string Performance { get; set; } = null!;
-        [MinLength(10), MaxLength(1000)]
-        public string? Interior { get; set; } = null!;
-        [MinLength(10), MaxLength(1000)]
+        [Required(ErrorMessage = "Required")]
+        [MinLength(10, ErrorMessage = "Must be above 10 symbols"), MaxLength(1000, ErrorMessage = "Must be below 1000 symbols")]
+        public string Interior { get; set; } = null!;
+        [MinLength(10, ErrorMessage = "Must be above 10 symbols"), MaxLength(1000, ErrorMessage = "Must be below 1000 symbols")]
         public string? Longevity { get; set; }
-        [MinLength(10), MaxLength(1000)]
+        [MinLength(10, ErrorMessage = "Must be above 10 symbols"), MaxLength(1000, ErrorMessage = "Must be below 1000 symbols")]
         public string? Features { get; set; }
         public ReviewType ReviewType { get; set; }
         public VehicleRating VehicleRating { get; set; }
         public string VehicleName { get; set; } = null!;
+        public ReviewPreviewModel? ReviewPreviewModel { get; set; }
         public ICollection<VehicleRating> VehicleRatings { get; set; }
     }
 }
