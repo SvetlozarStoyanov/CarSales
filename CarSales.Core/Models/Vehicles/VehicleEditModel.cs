c3cc7a0726361f49af2a672c99ca05aab3ffe889
SvetlozarStoyanov <svetlio214@gmail.com>
2023-08-25 22:20:06 +0300

    New model for editing vehicles

---
+++ CarSales.Core/Models/Vehicles/VehicleEditModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSales.Core.Models.Vehicles
+{
+    public class VehicleEditModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [MinLength(5), MaxLength(100)]
+        public string Description { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string? ImageUrl { get; set; } 
+        public string? DefaultImageUrl { get; set; } 
+        public decimal Price { get; set; }
+        public decimal OldPrice { get; set; }
+    }
+}
