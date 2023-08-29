85068f0f79277000118a8b2d99f67c0f92ef9e14
SvetlozarStoyanov <svetlio214@gmail.com>
2023-08-29 17:43:33 +0300

    Added test project to solution

---
+++ CarSales.Tests/UnitTests/ReviewServiceTests.cs
@@ -0,0 +1,73 @@
+﻿using AirsoftMatchMaker.Infrastructure.Data.Common.Repository;
+using CarSales.Core.Contracts;
+using CarSales.Core.Services;
+using CarSales.Infrastructure.Data;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Xunit;
+
+namespace CarSales.Tests.UnitTests
+{
+    public class ReviewServiceTests /*: IAsyncLifetime*/
+    {
+        private CarSalesDbContext context;
+        private IRepository repository;
+        private IDistributedCache cache;
+        private IReviewService reviewService;
+        private IContainer container;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            var contextOptions = new DbContextOptionsBuilder<CarSalesDbContext>()
+                .UseInMemoryDatabase("CarSalesTestDB")
+                .Options;
+
+            context = new CarSalesDbContext(contextOptions, false);
+
+            context.ApplyConfiguration = false;
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            repository = new Repository(context);
+
+            container = new ContainerBuilder()
+              // Set the image for the container to "testcontainers/helloworld:1.1.0".
+              .WithImage("redis:latest")
+              // Bind port 6379 of the container to a port 6379 on the host.
+              .WithPortBinding(6379, 6379)
+              // Wait until the HTTP endpoint of the container is available.
+              .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(6379))
+              // Build the container configuration.
+              .Build();
+
+            await container.StartAsync();
+            var redisOptions = new RedisCacheOptions();
+
+            redisOptions.InstanceName = "CarSales_";
+            
+            redisOptions.Configuration = "localhost:6379" ?? throw new InvalidOperationException("Connection string 'Redis' not found.");
+            cache = new RedisCache(redisOptions);
+
+            reviewService = new ReviewService(repository, cache);
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await container.StopAsync();
+        }
+
+        [Test]
+        public async Task Test_GetLatestReviewsAsync_ReturnsCorrectQuantityOfReviews()
+        {
+            var reviews = await reviewService.GetLatestReviewsAsync();
+            Assert.That(reviews.Count(), Is.EqualTo(3));
+        }
+
+    }
+}
---
+++ CarSales.Tests/UnitTests/VehicleServiceTests.cs
@@ -0,0 +1,83 @@
+﻿using AirsoftMatchMaker.Infrastructure.Data.Common.Repository;
+using CarSales.Core.Contracts;
+using CarSales.Core.Enums;
+using CarSales.Core.Services;
+using CarSales.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace CarSales.Tests.UnitTests
+{
+    public class VehicleServiceTests
+    {
+        private CarSalesDbContext context;
+        private IRepository repository;
+        private IDistributedCache cache;
+        private IVehicleService vehicleService;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            var contextOptions = new DbContextOptionsBuilder<CarSalesDbContext>()
+            .UseInMemoryDatabase("CarSalesTestDB")
+            .Options;
+
+            context = new CarSalesDbContext(contextOptions, false);
+
+            context.ApplyConfiguration = false;
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            repository = new Repository(context);
+
+            vehicleService = new VehicleService(repository);
+        }
+
+        [Test]
+        public async Task Test_GetVehiclesForSaleWithNoFiltersReturnsAllVehicles()
+        {
+            var vehicles = await vehicleService.GetAllVehiclesForSaleAsync();
+            Assert.That(vehicles.VehicleCount, Is.EqualTo(3));
+        }
+        [Test]
+        public async Task Test_GetVehiclesForSaleWithSearchTermReturnsAppropriateVehicles()
+        {
+            var vehicles = await vehicleService.GetAllVehiclesForSaleAsync("BMW");
+            Assert.That(vehicles.VehicleCount, Is.EqualTo(1));
+            vehicles = await vehicleService.GetAllVehiclesForSaleAsync("Audi");
+            Assert.That(vehicles.VehicleCount, Is.EqualTo(1));
+            vehicles = await vehicleService.GetAllVehiclesForSaleAsync("Opel");
+            Assert.That(vehicles.VehicleCount, Is.EqualTo(0));
+        }
+
+        [Test]
+        public async Task Test_GetVehiclesForSaleWithSetVehiclesPerPageReturnsAppropriateVehicles()
+        {
+            var vehicles = await vehicleService.GetAllVehiclesForSaleAsync(null, 2, 1);
+            Assert.That(vehicles.VehicleCount, Is.EqualTo(3));
+            Assert.That(vehicles.Vehicles.Count, Is.EqualTo(2));
+            vehicles = await vehicleService.GetAllVehiclesForSaleAsync(null, 2, 2);
+            Assert.That(vehicles.Vehicles.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task Test_GetVehiclesForSaleWithVehicleTypesReturnsAppropriateVehicles()
+        {
+            var vehicles = await vehicleService.GetAllVehiclesForSaleAsync(null, 6, 1, "Car");
+            Assert.That(vehicles.VehicleCount, Is.EqualTo(3));
+            vehicles = await vehicleService.GetAllVehiclesForSaleAsync(null, 6, 1, "Motorcycle");
+            Assert.That(vehicles.VehicleCount, Is.EqualTo(0));
+        }
+
+        [Test]
+        public async Task Test_GetVehiclesForSaleWithSortingReturnsAppropriateSortedVehicles()
+        {
+            var vehicles = await vehicleService.GetAllVehiclesForSaleAsync(null, 6, 1, "", VehicleSorting.RatingAscending);
+            Assert.That(vehicles.VehicleCount, Is.EqualTo(3));
+            Assert.That(vehicles.Vehicles.First().Name, Is.EqualTo("Bugatti Veyron 2011"));
+            vehicles = await vehicleService.GetAllVehiclesForSaleAsync(null, 6, 1, "", VehicleSorting.Alphabetically);
+            Assert.That(vehicles.Vehicles.First().Name, Is.EqualTo("Audi A6 2014"));
+        }
+    }
+}
