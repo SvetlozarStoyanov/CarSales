5de25d58e8832ed5aafebed8e6e5763f6e418efa
SvetlozarStoyanov <svetlio214@gmail.com>
2023-08-30 21:19:27 +0300

    Added new exception for when item is not for sale

---
+++ CarSales.Core/Exceptions/NotForSaleException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSales.Core.Exceptions
+{
+    public class NotForSaleException : Exception
+    {
+        public NotForSaleException(string? message) : base(message)
+        {
+
+        }
+    }
+}
