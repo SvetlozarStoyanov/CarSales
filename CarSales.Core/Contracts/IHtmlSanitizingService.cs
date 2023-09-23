14bdc5334b72d4c4076f8ba99de9c4cff075cd79
SvetlozarStoyanov <svetlio214@gmail.com>
2023-09-23 12:09:07 +0300

    Added Html sanitizing service which removes javascript code from strings

---
+++ CarSales.Core/Contracts/IHtmlSanitizingService.cs
@@ -0,0 +1,20 @@
+﻿namespace CarSales.Core.Contracts
+{
+    public interface IHtmlSanitizingService
+    {
+        /// <summary>
+        /// Removes all javascript code in object of type <typeparamref name="T"/> string properties
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        T SanitizeObject<T>(T obj);
+
+        /// <summary>
+        /// Removes javascript code from <paramref name="stringProperty"/> 
+        /// </summary>
+        /// <param name="stringProperty"></param>
+        /// <returns></returns>
+        string SanitizeStringProperty(string stringProperty);
+    }
+}
---
+++ CarSales.Core/Services/HtmlSanitizingService.cs
@@ -0,0 +1,38 @@
+﻿using CarSales.Core.Contracts;
+using Ganss.Xss;
+
+using System.Reflection;
+
+namespace CarSales.Core.Services
+{
+    public class HtmlSanitizingService : IHtmlSanitizingService
+    {
+        private readonly HtmlSanitizer sanitizer;
+        public HtmlSanitizingService()
+        {
+            sanitizer = new HtmlSanitizer();
+        }
+
+        public T SanitizeObject<T>(T obj)
+        {
+            Type type = typeof(T);
+            PropertyInfo[] properties = type.GetProperties().Where(p=> p.PropertyType.Name == "String").ToArray();
+            foreach (PropertyInfo property in properties)
+            {
+                string pName = property.Name;
+                object pValue = property.GetValue(obj, null);
+                if (pValue == null) 
+                    continue;
+                property.SetValue(obj, sanitizer.Sanitize(pValue.ToString()), null);
+            }
+            return obj;
+        }
+
+        public string SanitizeStringProperty(string? stringProperty)
+        {
+            if (stringProperty == null)
+                return null;
+            return sanitizer.Sanitize(stringProperty);
+        }
+    }
+}
