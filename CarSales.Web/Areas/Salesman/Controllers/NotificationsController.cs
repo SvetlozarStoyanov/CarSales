3b83580ef27528e1db056e4485192fb932a9bac9
SvetlozarStoyanov <svetlio214@gmail.com>
2023-11-17 16:13:27 +0200

    Added new notification controller and views

---
+++ CarSales.Web/Areas/Salesman/Controllers/NotificationsController.cs
@@ -0,0 +1,22 @@
+﻿using CarSales.Core.Contracts;
+using CarSales.Web.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSales.Web.Areas.Salesman.Controllers
+{
+    public class NotificationsController : BaseController
+    {
+        private readonly INotificationService notificationService;
+
+        public NotificationsController(INotificationService notificationService)
+        {
+            this.notificationService = notificationService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await notificationService.GetAllNotificationsAsync(User.Id());
+            return View(model);
+        }
+    }
+}
