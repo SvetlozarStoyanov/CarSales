7aa7a39eef447155a184981aa03a57fd6838597a
SvetlozarStoyanov <svetlio214@gmail.com>
2023-11-17 16:12:17 +0200

    Added new service which creates or reads notifications

---
+++ CarSales.Core/Contracts/INotificationService.cs
@@ -0,0 +1,13 @@
+﻿using CarSales.Core.Models.Notifications;
+
+namespace CarSales.Core.Contracts
+{
+    public interface INotificationService
+    {
+        Task CreateNotificationAsync(string userId, string title, string link);
+
+        Task<List<NotificationListModel>> GetAllNotificationsAsync(string userId);
+
+        Task<NotificationViewModel> GetNotificationByIdAsync(int id, string userId);
+    }
+}
---
+++ CarSales.Core/Services/NotificationService.cs
@@ -0,0 +1,70 @@
+﻿using AirsoftMatchMaker.Infrastructure.Data.Common.Repository;
+using CarSales.Core.Contracts;
+using CarSales.Core.Models.Notifications;
+using CarSales.Infrastructure.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarSales.Core.Services
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly IRepository repository;
+
+        public NotificationService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task CreateNotificationAsync(string userId, string title, string link)
+        {
+            var notification = new Notification()
+            {
+                UserId = userId,
+                Title = title,
+                Link = link
+            };
+
+            await repository.AddAsync<Notification>(notification);
+
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task<List<NotificationListModel>> GetAllNotificationsAsync(string userId)
+        {
+            var notifications = await repository.AllReadOnly<Notification>()
+                .Where(n => n.UserId == userId)
+                .Select(n => new NotificationListModel()
+                {
+                    Id = n.Id,
+                    Title = n.Title,
+                    Link = n.Link,
+                    IsRead = n.IsRead
+                })
+                .ToListAsync();
+
+            return notifications;
+        }
+
+        public async Task<NotificationViewModel> GetNotificationByIdAsync(int id, string userId)
+        {
+            var notification = await repository.GetByIdAsync<Notification>(id);
+            if (!notification.IsRead)
+            {
+                notification.IsRead = true;
+                await repository.SaveChangesAsync();
+            }
+            if (notification.UserId == userId)
+            {
+                var notificationViewModel = new NotificationViewModel()
+                {
+                    Id = notification.Id,
+                    Title = notification.Title,
+                    Link = notification.Link
+                };
+                return notificationViewModel;
+            }
+
+            return null!;
+        }
+    }
+}
