8ff3852fe73660ac000a9e84fbe45e2c8e0eb442
SvetlozarStoyanov <svetlio214@gmail.com>
2023-11-17 16:12:31 +0200

    Added new notification view models

---
+++ CarSales.Core/Models/Notifications/NotificationListModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CarSales.Core.Models.Notifications
+{
+    public class NotificationListModel
+    {
+        public int Id { get; init; }
+        public string Title { get; init; } = null!;
+        public string Link { get; init; } = null!;
+        public bool IsRead { get; set; }
+    }
+}
---
+++ CarSales.Core/Models/Notifications/NotificationViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CarSales.Core.Models.Notifications
+{
+    public class NotificationViewModel
+    {
+        public int Id { get; init; }
+        public string Title { get; init; } = null!;
+        public string Link { get; init; } = null!;
+    }
+}
