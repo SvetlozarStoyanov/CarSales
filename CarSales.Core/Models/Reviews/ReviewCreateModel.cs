3bec20d84a931dca31985c794965d47714797307
SvetlozarStoyanov <svetlio214@gmail.com>
2023-11-22 15:51:09 +0200

    Added salesman id property

--- CarSales.Core/Models/Reviews/ReviewCreateModel.cs
+++ CarSales.Core/Models/Reviews/ReviewCreateModel.cs
@@ -11,6 +11,7 @@ namespace CarSales.Core.Models.Reviews
             VehicleRatings = Enum.GetValues<VehicleRating>().Skip(1).ToHashSet();
         }
         public int Id { get; set; }
+        public int SalesmanId { get; set; }
         [Required(ErrorMessage = "Required")]
         [MinLength(10, ErrorMessage = "Must be above 10 symbols"), MaxLength(100, ErrorMessage = "Must be below 100 symbols")]
         public string Title { get; set; } = null!;
@@ -24,13 +25,13 @@ namespace CarSales.Core.Models.Reviews
         [MinLength(10, ErrorMessage = "Must be above 10 symbols"), MaxLength(1000, ErrorMessage = "Must be below 1000 symbols")]
         public string Interior { get; set; } = null!;
         [MinLength(10, ErrorMessage = "Must be above 10 symbols"), MaxLength(1000, ErrorMessage = "Must be below 1000 symbols")]
-        public string? Longevity { get; set; }
+        public string Longevity { get; set; }
         [MinLength(10, ErrorMessage = "Must be above 10 symbols"), MaxLength(1000, ErrorMessage = "Must be below 1000 symbols")]
-        public string? Features { get; set; }
+        public string Features { get; set; }
         public ReviewType ReviewType { get; set; }
         public VehicleRating VehicleRating { get; set; }
         public string VehicleName { get; set; } = null!;
-        public ReviewPreviewModel? ReviewPreviewModel { get; set; }
+        public ReviewPreviewModel ReviewPreviewModel { get; set; }
         public ICollection<VehicleRating> VehicleRatings { get; set; }
     }
 }
