8aff79fe432599f2c016f70edafd4264440dc0d3
SvetlozarStoyanov <svetlio214@gmail.com>
2023-11-24 18:45:24 +0200

    Minor changes

---
+++ CarSales.Web/wwwroot/js/Notifications/notificationsLoginPartial.js
@@ -0,0 +1,12 @@
+﻿const notificationsListItem = document.querySelector('#notificationsListItem');
+const notificationsList = document.querySelector('#notificationsList');
+const areaInput = document.querySelector('#areaInput');
+
+notificationsListItem.addEventListener('click', function () {
+    if (notificationsList.classList.contains('show')) {
+        $(notificationsList).load(`/${areaInput.value}/Notifications/GetNotificationsPartial`);
+        console.log('get request');
+    }
+})
+
+
