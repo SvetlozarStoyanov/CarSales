e3f22c62de5b827d87576b679a716cff9fee99b1
SvetlozarStoyanov <svetlio214@gmail.com>
2023-11-24 18:46:36 +0200

    Added js which uses jquery to fetch user notifications from notifications controller

--- CarSales.Web/wwwroot/js/Notifications/notificationsLoginPartial.js
+++ CarSales.Web/wwwroot/js/Notifications/notificationsLoginPartial.js
@@ -1,6 +1,6 @@
-﻿const notificationsListItem = document.querySelector('#notificationsListItem');
+﻿const areaInput = document.querySelector('#areaInput');
+const notificationsListItem = document.querySelector('#notificationsListItem');
 const notificationsList = document.querySelector('#notificationsList');
-const areaInput = document.querySelector('#areaInput');
 
 notificationsListItem.addEventListener('click', function () {
     if (notificationsList.classList.contains('show')) {
