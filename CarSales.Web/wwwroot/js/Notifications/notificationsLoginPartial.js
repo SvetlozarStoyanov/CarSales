69aca6e0abb103ac0c792d47dfcda0fe21119d2a
SvetlozarStoyanov <svetlio214@gmail.com>
2023-12-05 18:11:52 +0200

    Now updates notifications bell icon when user has unread notifications

--- CarSales.Web/wwwroot/js/Notifications/notificationsLoginPartial.js
+++ CarSales.Web/wwwroot/js/Notifications/notificationsLoginPartial.js
@@ -1,12 +1,35 @@
 ﻿const areaInput = document.querySelector('#areaInput');
+const notificationsIcon = document.querySelector('#notificationsIcon');
 const notificationsListItem = document.querySelector('#notificationsListItem');
 const notificationsList = document.querySelector('#notificationsList');
+
+window.addEventListener('load', async function () {
+    await checkIfUserHasUnreadNotifications();
+
+    setInterval(checkIfUserHasUnreadNotifications, 30000)
+
+})
 
 notificationsListItem.addEventListener('click', function () {
     if (notificationsList.classList.contains('show')) {
         $(notificationsList).load(`/${areaInput.value}/Notifications/GetNotificationsPartial`);
-        console.log('get request');
     }
 })
+
+async function checkIfUserHasUnreadNotifications() {
+    await fetch(`/${areaInput.value}/Notifications/DoesUserHaveUnreadNotifications`, {
+        method: 'GET',
+    })
+        .then(res => res.json())
+        .then(body => {
+            if (body === false) {
+                notificationsIcon.classList.remove('bi-bell-fill');
+                notificationsIcon.classList.add('bi-bell');
+            } else {
+                notificationsIcon.classList.add('bi-bell-fill');
+                notificationsIcon.classList.remove('bi-bell');
+            }
+        });
+}
 
 
