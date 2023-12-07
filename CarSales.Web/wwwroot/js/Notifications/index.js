b7cde54f6e664c5b7c24d20fb2f4a28914ad4657
SvetlozarStoyanov <svetlio214@gmail.com>
2023-12-07 18:23:19 +0200

    New js file for notifications index view

---
+++ CarSales.Web/wwwroot/js/Notifications/index.js
@@ -0,0 +1,19 @@
+﻿const markAsReadBtns = document.querySelectorAll('.mark-as-read-button');
+
+markAsReadBtns.forEach(b => b.addEventListener('click', async function (e) {
+    let currBtn = e.currentTarget;
+    if (!currBtn.disabled) {
+        let btnParent = currBtn.parentNode;
+        let notificationId = btnParent.parentNode.querySelector('.notification-id').value;
+        await fetch(`./Notifications/MarkAsRead/${notificationId}`);
+
+        currBtn.classList.remove('btn-warning');
+        currBtn.classList.add('btn-primary');
+        currBtn.disabled = true;
+
+        btnParent.classList.remove('bg-info');
+        btnParent.classList.remove('bg-opacity-50');
+        btnParent.classList.add('bg-secondary');
+        btnParent.classList.add('bg-opacity-10');
+    }
+}))
