195efe0791f7ab7544d173a46afb3da6b7588125
SvetlozarStoyanov <svetlio214@gmail.com>
2023-12-13 17:48:55 +0200

    Improvements to scrolling, now uses scroll event instead of scrollend event. Unzooming browser now loads all notifications

--- CarSales.Web/wwwroot/js/Notifications/index.js
+++ CarSales.Web/wwwroot/js/Notifications/index.js
@@ -1,38 +1,63 @@
-﻿const notificationsSection = document.querySelector('#notificationsSection');
+﻿const notificationsList = document.querySelector('#notificationsList');
+let timer;
 let allNotificationsLoaded = false;
 
 window.addEventListener('load', async function () {
     await loadMoreNotifications();
-});
-
-window.addEventListener('scrollend', async function () {
-    if (!allNotificationsLoaded) {
-        await loadMoreNotifications();
+    let height = window.innerHeight;
+    if (height >= 1070) {
+        while (!allNotificationsLoaded) {
+            await loadMoreNotifications();
+        }
     }
 });
 
-async function loadMoreNotifications() {
-    let skipped = notificationsSection.querySelectorAll('article').length;
+window.addEventListener('scroll', async function () {
+    if (timer) {
+        window.clearTimeout(timer);
+    }
+    timer = window.setTimeout(async function () {
+        let windowHeight = $(window).height();;
+        let windowScrollTop = $(window).scrollTop();
+        let scrollTopAndHeight = windowHeight + windowScrollTop;
+        let documentHeight = getDocHeight();
+        if (scrollTopAndHeight >= documentHeight - 200 && !allNotificationsLoaded) {
+            await loadMoreNotifications();
+        }
+    }, 100)
 
-    $.ajax({
+});
+
+window.addEventListener('resize', async function (e) {
+    let height = window.innerHeight;
+    let test = 0;
+    if (height >= 1070) {
+        while (!allNotificationsLoaded) {
+            await loadMoreNotifications();
+        }
+    }
+})
+
+async function loadMoreNotifications() {
+    let skipped = notificationsList.querySelectorAll('.notification-list-item').length;
+    await $.ajax({
         type: 'GET',
         url: `./Notifications/GetNotifications`,
         data: jQuery.param({ skipped: skipped }),
-        success: function (viewText) {
+        success: async function (viewText) {
             let viewHtml = jQuery.parseHTML(viewText);
             let markAsReadButtons = $(viewHtml).find('.mark-as-read-button');
-
             if (markAsReadButtons.length > 0) {
                 markAsReadButtons.each(function () {
                     $(this).on('click', markAsReadButtonClick);
                 });
             }
 
-            $(notificationsSection).append(viewHtml);
-            let currentNotificationCount = notificationsSection.querySelectorAll('article').length;
+            await $(notificationsList).append(viewHtml);
+            let currentNotificationCount = notificationsList.querySelectorAll('.notification-list-item').length;
             if (currentNotificationCount === 0) {
                 allNotificationsLoaded = true;
-                let noNotificationsHeading = notificationsSection.querySelector('#noNotificationsHeading');
+                let noNotificationsHeading = notificationsList.querySelector('#noNotificationsHeading');
                 noNotificationsHeading.classList.remove('d-none');
                 return;
             }
@@ -40,11 +65,12 @@ async function loadMoreNotifications() {
                 allNotificationsLoaded = true;
                 return;
             }
+        },
+        failure: function () {
+            console.log('Notifications get request failed')
         }
-    }
-    );
+    });
 }
-
 
 async function markAsReadButtonClick(e) {
     let currBtn = e.currentTarget;
@@ -65,4 +91,11 @@ async function markAsReadButtonClick(e) {
     }
 }
 
-
+function getDocHeight() {
+    var D = document;
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    );
+}
