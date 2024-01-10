332eea5bdab30372dd664aaafe75be29a6757863
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-10 18:15:45 +0200

    Added new model used in new vehicle buy partial view

---
+++ CarSales.Core/Models/Vehicles/VehicleBuyModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CarSales.Core.Models.Vehicles
+{
+    public class VehicleBuyModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+    }
+}
