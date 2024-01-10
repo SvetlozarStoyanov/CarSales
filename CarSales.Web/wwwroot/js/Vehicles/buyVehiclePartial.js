221223be8ee1eb67bfb8cdf6053a559d310bc465
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-10 18:18:50 +0200

    Added js file for new buy vehicle partial views

---
+++ CarSales.Web/wwwroot/js/Vehicles/buyVehiclePartial.js
@@ -0,0 +1,12 @@
+﻿const cancelBuyBtn = document.querySelector('#cancelBuyBtn');
+const creditsAfterBuyingInput = document.querySelector('#creditsAfterBuyingInput');
+const buyPrice = parseFloat(document.querySelector('#buyPrice').value);
+const userCurrentCredits = parseFloat(document.querySelector('#userCredits').textContent);
+creditsAfterBuyingInput.value = (userCurrentCredits - buyPrice).toFixed(2);
+
+//document.addEventListener('load', function () {
+//});
+
+cancelBuyBtn.addEventListener('click', function () {
+    e.preventDefault();
+})
