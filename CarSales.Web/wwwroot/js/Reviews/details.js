711d30d9b4e717eb326a777072c0d95b9b64b375
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-11 21:31:56 +0200

    New js file which adds event listeners for hovering on vehicle image. Also triggers those events when page is loaded

---
+++ CarSales.Web/wwwroot/js/Reviews/details.js
@@ -0,0 +1,36 @@
+﻿const reviewImageAnchorTags = document.querySelectorAll('.custom-review-image-anchor');
+
+
+
+window.addEventListener('load', function (e) {
+
+    //reviewImageAnchorTags.forEach(achorTag => achorTag.addEventListener('mouseenter', function (e) {
+    //    const backgroundDiv = e.currentTarget.querySelector('.custom-review-image-background-div');
+    //    backgroundDiv.classList.add('custom-review-image-background-div-show');
+    //}));
+
+    //reviewImageAnchorTags.forEach(achorTag => achorTag.addEventListener('mouseleave', function (e) {
+    //    const backgroundDiv = e.currentTarget.querySelector('.custom-review-image-background-div');
+    //    backgroundDiv.classList.remove('custom-review-image-background-div-show');
+    //}));
+
+    reviewImageAnchorTags.forEach(anchorTag => {
+        anchorTag.addEventListener('mouseenter', function (e) {
+            const backgroundDiv = e.currentTarget.querySelector('.custom-review-image-background-div');
+            backgroundDiv.classList.add('custom-review-image-background-div-show');
+        });
+        anchorTag.addEventListener('mouseleave', function (e) {
+            const backgroundDiv = e.currentTarget.querySelector('.custom-review-image-background-div');
+            backgroundDiv.classList.remove('custom-review-image-background-div-show');
+        })
+        setTimeout(() => {
+            anchorTag.dispatchEvent(new Event('mouseenter'))
+        }, 1500);
+
+        setTimeout(() => {
+            anchorTag.dispatchEvent(new Event('mouseleave'))
+        }, 5000);
+    });
+
+})
+
